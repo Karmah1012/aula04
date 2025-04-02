052e4ff38e4343d663fef6608202460f9b906dbb
Karmah <karmah1012@gmail.com>
2025-04-02 14:43:52 -0300

    adicionado a opçao e ou g

--- exercicio01.py
+++ exercicio01.py
@@ -5,13 +5,13 @@ combustivel = input("Digite seu combustivel \n"
 vGas = 5.8
 vEta = 4.9
 
-if combustivel == "G":
+if combustivel == "G" or combustivel == "g":
     valorgasolina = litrocombustivel * vGas
-    print(f"Voce abasteceu {litrocombustivel} e gastou {valorgasolina} de gasolina")
+    print(f"Voce abasteceu {litrocombustivel} litros e gastou {valorgasolina} reais")
 else:
-    if combustivel == "E":
+    if combustivel == "E" or combustivel == "e":
         valoretanol = litrocombustivel * vEta
-        print(f"Voce abasteceu {litrocombustivel} e gastou {valoretanol} de etanol")
+        print(f"Voce abasteceu {litrocombustivel} e gastou {valoretanol} reais")
 
 
 
